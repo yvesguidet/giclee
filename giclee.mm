1bbe67e86ddeabee969f274492b41670bda732a8
Yves Guidet <yves.guidet@gmail.com>
2016-05-05 17:15:56 +0200

    modifié:         .gitignore 	modifié:         giclee.mm 	modifié:         giclee.py

--- giclee.mm
+++ giclee.mm
@@ -2,8 +2,9 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1462443364847" ID="ID_1106136207" MODIFIED="1462458156021" TEXT="plan gicl&#xe9;e">
 <node CREATED="1462458043580" ID="ID_890761760" LINK="/home/yves/2011/2016/printemps/info/mmZero/giclee/" MODIFIED="1462458493739" POSITION="left" TEXT="dir"/>
-<node CREATED="1462458562217" ID="ID_533803954" MODIFIED="1462458578285" POSITION="right" TEXT="Ascension">
+<node CREATED="1462458562217" ID="ID_533803954" LINK="/home/yves/2011/2016/printemps/cartes/Ascension16.mm" MODIFIED="1462459481081" POSITION="right" TEXT="Ascension">
 <cloud/>
 </node>
+<node CREATED="1462459304622" ID="ID_682719383" LINK="zisse2016/zisse2016.mm" MODIFIED="1462459304624" POSITION="right" TEXT="(get)zisse2016"/>
 </node>
 </map>
--- giclee.py
+++ giclee.py
@@ -2,6 +2,10 @@
 # alias n='newMap.pl'
 
 l = ('lun', 'mar', 'mer', 'jeu')
+sem = '19'
+i = 9
 
 for j in l:
-	pass
+	#	util. str.format()
+	k = 'newMap.pl ' + j + str(i) + '-' + sem
+	print k
