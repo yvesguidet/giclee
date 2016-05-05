3c1e1d002c0688e1bb3c1b5525b86b2b42135ef4
Yves Guidet <yves.guidet@gmail.com>
2016-05-05 16:38:32 +0200

    nouveau fichier: .gitignore 	nouveau fichier: giclee.mm 	nouveau fichier: giclee.py

---
+++ giclee.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1462443364847" ID="ID_1106136207" MODIFIED="1462458156021" TEXT="plan gicl&#xe9;e">
+<node CREATED="1462458043580" ID="ID_890761760" LINK="/home/yves/2011/2016/printemps/info/mmZero/giclee/" MODIFIED="1462458493739" POSITION="left" TEXT="dir"/>
+<node CREATED="1462458562217" ID="ID_533803954" MODIFIED="1462458578285" POSITION="right" TEXT="Ascension">
+<cloud/>
+</node>
+</node>
+</map>
---
+++ giclee.py
@@ -0,0 +1,7 @@
+#!  /usr/bin/env python
+# alias n='newMap.pl'
+
+l = ('lun', 'mar', 'mer', 'jeu')
+
+for j in l:
+	pass
