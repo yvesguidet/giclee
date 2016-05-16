641a8a599ac359360bc2fc41b8c85cf0b046ec92
Yves Guidet <yves.guidet@gmail.com>
2016-05-16 10:47:50 +0200

    modifié:         giclee.py

--- giclee.py
+++ giclee.py
@@ -1,12 +1,14 @@
 #!  /usr/bin/env python
 # alias n='newMap.pl'
 
-l = ('lun', 'mar', 'mer', 'jeu')
-sem = '19'
-i = 9
+l = ('lun', 'mar', 'mer', 'jeu', 'ven', 'sam', 'dim')
+sem = '20'
+quantieme = 16
 sep = '_'	# mieux que '-' pour modif
 
 for j in l:
 	#	util. str.format()
-	k = 'newMap.pl ' + j + str(i) + '_' + sem
+	#	mettre un format
+	k = 'newMap.pl ' + j + str(quantieme) + '_sem' + sem
+	quantieme += 1
 	print k
