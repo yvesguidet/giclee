8968684241515de9760ceb8f25073fae9961bb89
Yves Guidet <yves.guidet@gmail.com>
2016-06-04 10:13:30 +0200

    modifié:         giclee.py

--- giclee.py
+++ giclee.py
@@ -2,13 +2,18 @@
 # alias n='newMap.pl'
 
 l = ('lun', 'mar', 'mer', 'jeu', 'ven', 'sam', 'dim')
-sem = '20'
-quantieme = 16
+sem = '23'
+quantieme = 6
 sep = '_'	# mieux que '-' pour modif
+
+import os
 
 for j in l:
 	#	util. str.format()
 	#	mettre un format
 	k = 'newMap.pl ' + j + str(quantieme) + '_sem' + sem
 	quantieme += 1
+	if quantieme > 31:
+		quantieme = 1
 	print k
+	os.system(k)
