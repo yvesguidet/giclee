1a28156b671d0299c9d71ce2f52dd1b6a701434e
Yves Guidet <yves.guidet@gmail.com>
2016-11-02 07:37:56 +0100

    nbJMois 	modifié:         majParms.py

--- majParms.py
+++ majParms.py
@@ -8,6 +8,20 @@ print "d = {}".format(d)
 
 quantieme2Day = str.split(d)[1]
 print "quantieme2Day = {}".format(quantieme2Day)
+mois = str.split(d)[2]
+
+nbJours = {'novembre': 30} # compléter ;)
+
+print "nbJours = {}".format(nbJours)
+
+print "mois = {}".format(mois)
+nbJMois = nbJours[mois]
+
+print "nbJMois = {}".format(nbJMois)
+
+print "écriture de quantieme2Day = {} dans params.py".format(quantieme2Day)
+print "écriture de nbJMois = {} dans params.py".format(nbJMois)
 params = open('params.py', "a")
-print "finir, écrire ds params.py"
+print "finir"
 print >>params, "quantieme2Day = {}".format(quantieme2Day)
+print >>params, "nbJMois = {}".format(nbJMois)
