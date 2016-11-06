4f9216bf6c20de3dce0314ce4ef98d84e0552f26
Yves Guidet <yves.guidet@gmail.com>
2016-11-06 12:10:27 +0100

    nouveau fichier: semCour.py

---
+++ semCour.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from sys import path
+
+path.append('/home/yves/2011/2016/automne/rentree/pymmPremCrochet')
+
+from FinProvisoire import FinProvisoire
+from os import system
+
+def semCour():
+	raise FinProvisoire()
