b437150118cd30ddb8ee55ca9e67154c62f93603
Yves Guidet <yves.guidet@gmail.com>
2018-11-01 07:49:22 +0100

    on part du 28 	modifié:         njm.py

--- njm.py
+++ njm.py
@@ -3,6 +3,18 @@
 
 def njm(m, a = 2018):
 	'''nb jours du mois'''
+	#
+	import datetime
+	d = datetime.date.today()	# auj
+	d = d.replace(month = m, year = a, day = 28)
+	print 'd = {}'.format(d)
+	assert 0, 'use replace ok ?'
+
+	while 1:
+		d = dem1(d)
+		print 'd = {}'.format(d)
+		print 'type(d) = {}'.format(type(d))
+		assert 0, 'finir'
 	assert 0, 'finir'
 
 if  __name__ == '__main__':
